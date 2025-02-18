0cb32260312dd1f151a605c1e9b7676dc329eabb
Avinash Ingale <avinashingale912@gmail.com>
2025-02-18 11:07:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,54 @@
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+let section = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    section.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height){
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + ']').classList.add('active')
+            })
+        }
+    })
+}
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+
+
+document.getElementById('contact').addEventListener('submit', function(event) {
+    event.preventDefault();
+    
+    alert('Thank you for contacting me! I will respond shortly.');
+
+    // Clear input fields after submission
+    document.querySelectorAll(".contact input, .contact textarea").forEach(input => {
+        input.value = "";
+    });
+});
+
+
+document.querySelectorAll('.navbar a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        const targetId = this.getAttribute('href').substring(1);
+        const targetSection = document.getElementById(targetId);
+
+        if (targetSection) {
+            window.scrollTo({
+                top: targetSection.offsetTop - 50, // Adjust this value to offset the fixed navbar
+                behavior: 'smooth'
+            });
+        }
+    });
+});
